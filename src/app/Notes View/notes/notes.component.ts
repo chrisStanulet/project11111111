8ea3206bd3ce61dfb458da7e9caf2cfbf74b6431
chrisStanulet <chrisstanulet@gmail.com>
2019-04-03 17:57:55 -0400

    1 file modified

--- src/app/Notes View/notes/notes.component.ts
+++ src/app/Notes View/notes/notes.component.ts
@@ -9,36 +9,31 @@ import { DataService } from './data.service';
 })
 export class NotesComponent implements OnInit {
 
-noteLocation1:string;
+    public noteLocation1:string;
     constructor(public dataService: DataService) {  
-      this.dataService.setData(noteLocation1);
     }  
 
     Biology(){
       this.noteLocation1 ="Biology"
-      this.dataService.setData(noteLocation1);
+      this.dataService.setData(this.noteLocation1);
 
     }
     History(){
       this.noteLocation1 ="Histroy"
-      this.dataService.setData(noteLocation1);
+      this.dataService.setData(this.noteLocation1);
       
     }
     Maths(){
       this.noteLocation1 ="Maths"
-      this.dataService.setData(noteLocation1);
+      this.dataService.setData(this.noteLocation1);
     }
     English(){
       this.noteLocation1 ="English"
-      this.dataService.setData(noteLocation1);
+      this.dataService.setData(this.noteLocation1);
     }
 
 
   ngOnInit() {
   }
-
-}
-class noteLocation1
-{
 
 }

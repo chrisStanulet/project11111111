394e7714ef9f6a4d2246274ba3ad5525baaa70a7
Chris <chrisstanulet@gmail.com>
2019-04-18 15:14:43 -0400

    Theres a prety cool calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppComponent, BottomSheetOverviewExampleSheet } from './app.component';
 
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AppRoutingModule } from './app-routing.module';
-
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
 
 import {
   
@@ -62,6 +66,12 @@ import { DataService } from '../data.service';
 @NgModule({
   imports:      [ 
   HttpClientModule,
+  NgbModalModule,
+  CommonModule,
+  CalendarModule.forRoot({
+    provide: DateAdapter,
+    useFactory: adapterFactory}),
+    FlatpickrModule.forRoot(),
   BrowserAnimationsModule,
   AppRoutingModule,
   BrowserModule, 
